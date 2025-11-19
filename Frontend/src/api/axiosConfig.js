05b676d23c406ea0d49fc02a8a7eddb20849b254
parv1601 <Parv.sharma@iiitb.ac.in>
2025-11-19 11:46:23 +0530

    fixed env

--- Frontend/src/api/axiosConfig.js
+++ Frontend/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const base = import.meta.env.VITE_API_URL || "https://promptly-3gli.onrender.com/"; // uses Vite env var
+const base = process.env.VITE_API_URL; // uses Vite env var
 const api = axios.create({
   baseURL: base,
 });
